e7cbfa90f6fabcff843ff919aacd08a344484123
samdmarshall <me@samdmarshall.com>
2014-08-14 12:20:09 -0400

    fixing up printing behavior

--- _print.py
+++ _print.py
@@ -2,41 +2,50 @@ import _environment
 import _colours
 
 
+TYPE_KEY = 'type';
+DATA_KEY = 'data';
+COLOUR_KEY = 'colour';
+STRING_KEY = 'string';
+FORMATTER_KEY = 'formatter';
+DISPLAY_KEY = 'display';
+STATE_KEY = 'state';
+
+
 def fmt(args):
-    line = "";
+    line = '';
     for item in args:
-        item_type = item['type'];
-        item_data = item['data'];
-        if item_type == 'colour':
-            colour_value = item_data['colour'];
-            state_value = item_data['state'];
+        item_type = item[TYPE_KEY];
+        item_data = item[DATA_KEY];
+        if item_type == COLOUR_KEY:
+            colour_value = item_data[COLOUR_KEY];
+            state_value = item_data[STATE_KEY];
             line += _colours.cmap(colour_value,state_value);
-        elif item_type == 'string':
-            formatter_value = item_data['formatter'];
-            display_value = item_data['display'];
+        elif item_type == STRING_KEY:
+            formatter_value = item_data[FORMATTER_KEY];
+            display_value = item_data[DISPLAY_KEY];
             line += formatter_value % display_value;
         else:
-            line += "";
+            line += '';
     
     print line;
 
 
 def Colour(name, state):
     return {
-        'type': 'colour',
-        'data': {
-            'colour': name,
-            'state': state
+        TYPE_KEY: COLOUR_KEY,
+        DATA_KEY: {
+            COLOUR_KEY: name,
+            STATE_KEY: state
         }
     };
 
 
 def String(formatter, value):
     return {
-        'type': 'string',
-        'data': {
-            'formatter': formatter,
-            'display': value
+        TYPE_KEY: STRING_KEY,
+        DATA_KEY: {
+            FORMATTER_KEY: formatter,
+            DISPLAY_KEY: value
         }
     };
 
